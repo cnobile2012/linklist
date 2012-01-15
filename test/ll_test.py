59ea3e31aba0e9c834b866c47dd10d1904758ee0
cnobile <cnobile>
2012-01-15 06:10:11 +0000

    Changed the test to work on more version of Python.

--- test/ll_test.py
+++ test/ll_test.py
@@ -75,27 +75,19 @@ class TestLibDll(unittest.TestCase):
         """
         msg = "Invalid Info type is not a subclass of ctypes Structure."
 
-        try: # Python >= 2.7
-            self.assertNotIsInstance(self._dll.checkInfoType(Info()),
-                                     APIException, msg)
-        except AttributeError:
-            try: # Python <= 2.6
-                self._dll.checkInfoType(Info())
-            except:
-                self.fail(msg)
+        try:
+            self._dll.checkInfoType(Info())
+        except:
+            self.fail(msg)
 
         class BadInfo(object):
             pass
 
-        try: # Python >= 2.7
-            self.assertIsInstance(self._dll.checkInfoType(BadInfo()),
-                                  APIException, msg)
-        except AttributeError:
-            try: # Python <= 2.6
-                self._dll.checkInfoType(BadInfo())
-                self.fail(msg)
-            except APIException:
-                pass
+        try:
+            self._dll.checkInfoType(BadInfo())
+            self.fail(msg)
+        except APIException:
+            pass
 
     def test_sizeofList(self):
         """
